11b62dd3cfeb01415c6a3c4252afcaa48b19627a
unknown <himangahu.zoadder@gmail.com>
2018-08-26 18:45:37 -0400

    add something

---
+++ git_project/src/git_1/Git_First_push.java
@@ -0,0 +1,15 @@
+package git_1;
+
+public class Git_First_push {
+
+	public static void main(String[] args) {
+		String s="How are you";
+		String s1[]=s.split(" ");
+		for(int i=s1.length-1;i>=0;i--)
+		{
+			System.out.println(s1[i]);
+		}
+
+	}
+
+}
